b299b6afab946704cbde4e42638407205cfa8823
soninlaw <jonsoninlaw@gmail.com>
2020-02-04 19:40:19 +0100

    Page layout and basket details showing

--- src/main/java/com/yolopizza/Yolo/Pizza/controller/PizzaController.java
+++ src/main/java/com/yolopizza/Yolo/Pizza/controller/PizzaController.java
@@ -1,6 +1,7 @@
 package com.yolopizza.Yolo.Pizza.controller;
 
 import com.yolopizza.Yolo.Pizza.entity.Basket;
+import com.yolopizza.Yolo.Pizza.entity.BasketPizza;
 import com.yolopizza.Yolo.Pizza.entity.Pizza;
 import com.yolopizza.Yolo.Pizza.repository.BasketRepository;
 import com.yolopizza.Yolo.Pizza.repository.PizzaRepository;
@@ -24,18 +25,30 @@ public class PizzaController {
 
     @GetMapping("/")
     public String index(Model model,
-                        @RequestParam(required = false) Long basketId) {
+                        @RequestParam(defaultValue = "0") Long basketId) {
 
         Basket basket = new Basket();
-        if (basketId != null) {
-            Optional<Basket> optionalBasket = basketRepository.findById(basketId);
-            if (optionalBasket.isPresent()) {
-                basket = optionalBasket.get();
+        int quantity = 0;
+        double amount = 0d;
+        Optional<Basket> optionalBasket = basketRepository.findById(basketId);
+        if (optionalBasket.isPresent()) {
+            basket = optionalBasket.get();
+            quantity = basket.getBasketPizzas().size();
+            for (BasketPizza basketPizza : basket.getBasketPizzas()) {
+                amount+= basketPizza.getPizza().getPrice();
             }
         }
-        model.addAttribute("basket", basket);
+        model.addAttribute("quantity", quantity);
+        model.addAttribute("amount", amount);
+        model.addAttribute("basketId", basketId);
         List<Pizza> pizzas = pizzaRepository.findAll();
         model.addAttribute("pizzas", pizzas);
         return "index";
+    }
+
+    @GetMapping("/buy")
+    public String buy(@RequestParam Long pizzaId,
+                      @RequestParam Long basketId) {
+        return "redirect:/?basketId=" + basketId;
     }
 }
